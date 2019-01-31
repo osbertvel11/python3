904c4ea5e8e6fc7281c248e1ec54aab8d9166012
osbertvel11 <osbert.vel1@gmail.com>
2019-01-30 23:43:13 -0600

    dolar a quetzales

---
+++ ejemplo1dolar.py
@@ -0,0 +1,29 @@
+#osbert
+#crear un program que les permita seleccionar entre una de dos opcions
+#convertir dolares a quetzales o quetzales a dolares
+
+print ("BIENVENIDO AL SISTEMA".center(50,"*"))
+
+print ("sleccionar opcion\n")
+print ("1.quetzales a dolares:\n")
+print ("2.dolares a quetzales:")
+
+
+op = 0
+op = int(input())
+valdolar = 7.71
+
+if op == 1:
+    cant = float(input("ingrese la cantidad:"))
+    total = (cant/valdolar)
+    print("conversion: ","$.",total)
+          
+elif  op == 2:
+     cant = float(input("ingrese la cantidad:"))
+     total = (cant*valdolar)
+     print("conversion: ","Q.",total)
+
+else:
+          print("opcion no valida:")
+          
+          
---
+++ ejercicos.py
@@ -0,0 +1,13 @@
+#osbert
+#1
+#crear un program que les permita seleccionar entre una de dos opcions
+#convertir dolares a quetzales o quetzales a dolares
+
+
+ #2
+#solicitar al usuario que escoja una de las siguientes opciones
+#sumar dos numeros #
+#restrar tres numeros
+#mjltiplicar cuatro numeros
+#dividir dis umeros
+#y si el usuraio ingresa una opcion invalida hacercelo saber
